7fee08b45ba40c21650f022f174a60fb5c5a05d5
lingesh-sk <grasplee@gmail.com>
2023-01-13 16:51:32 +0530

    third commit

--- src/React-Routing/home.js
+++ src/React-Routing/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./about";
 import Contact from "./contact";
 import Navs from "./navbar";
